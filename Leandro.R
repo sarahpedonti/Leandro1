a30a17a0b0faf860d0a5e8fc143cb55b9f21c958
sarahpedonti <86248316+sarahpedonti@users.noreply.github.com>
2021-08-14 15:03:57 -0400

    updates
    updates

--- Leandro.R
+++ Leandro.R
@@ -1,109 +1,168 @@
-library(shiny)
-#download Leandro data
-Leandro <- read.csv("~/Downloads/2021LeandroCostEstimateswv.csv")
-library(shinydashboard)
-#basic architecture for shiny app
+#download Leandro data (only need if not working from Github and reading data locally)
+#Leandro <- read.csv("~/Downloads/2021LeandroCostEstimateswv.csv")
+read.csv("2021Leandrocostestimatesfullfund_wGEOID.csv")
+read.csv("2027Leandrocostestimateswfullfunding_wGEOID.csv")
+library(sf)
+library(dplyr)
+#readingin and merging shape file and finance info (no longer need after initial creation)
+#county_info <- read.csv("~/Leandro1/2021LeandroCostEstimateswv_wGEOID.csv")
+#county_info$GEOID <- as.character(county_info$GEOID)
+#county_shape <- read_sf("~/Leandro1/tl_2017_37_unsd.shp")
+#county_info %>% full_join(y = county_shape, by = 'GEOID') %>%
+  #st_as_sf() %>%
+  #sf::st_write("~/Leandro1/output.shp") 
 
-ui <- dashboardPage(
-  dashboardHeader(title = "Leandro Budget Tool"),
-  dashboardSidebar(),
-  dashboardBody(leaflet(mergedshapeL) %>%
-                  addProviderTiles("HikeBike.HikeBike") %>%
-                  addPolygons(fillColor = "white",
-                              color = "black",
-                              weight = 0.5, 
-                              popup = paste(mergedshapeL$NAME, "<br>",
-                                            "Textbooks", mergedshapeL$Textbks, "<br>",
-                                            "At-risk", mergedshapeL$At_Risk, "<br>",
-                                            "Classroom Supply/Mat.", mergedshapeL$C_S___M, "<br>",
-                                            "LEP", mergedshapeL$Lmt_E_P, "<br>",
-                                            "Chi. w/ Dis.", mergedshapeL$Chld__D))),)
+#reading in merged shape file that was written from above
+mergedshapeL <- read_sf("output.shp")
+
+#get rid of commas so that R can read as.numeric
+install.packages("eeptools")
+library(eeptools)
+mergedshapeL$Total <- decomma(mergedshapeL$Total)
+
+
+library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(Rcpp)
+library(htmlwidgets)
+library(htmltools)
+
+#set color bins for chloropeth mapping
+library(RColorBrewer)
+pal <- colorNumeric(
+  palette = "Blues", 
+  domain = mergedshapeL$Total)
+library(colorspace)
+library(rgdal)
+
+
+
+
+#basic architecture for shiny app (with chloropeth mapping; change this to PPE?)
+
+ui <- fluidPage(#theme="bootstrap.css",
+  tags$head(
+    tags$style(HTML(" @import url('http://fonts.cdnfonts.com/css/itc-avant-garde-gothic-std-book');
+      body {
+        background-color: #1E355E;
+        color: white;
+      }
+      /* Change font of header text */
+      h2 {
+          font-family: 'ITC Avant Garde Gothic Std Bold', sans-serif;
+      }
+      /* Make text visible on inputs */
+      .shiny-input-container {
+        color: #F99D20;
+      }"))
+  ),
+  tag.map.title <- tags$style(HTML("
+  .leaflet-control.map-title { 
+    transform: translate(-50%,20%);
+    position: fixed !important;
+    left: 50%;
+    text-align: center;
+    padding-left: 10px; 
+    padding-right: 10px; 
+    background: #1E355E;
+    font-weight: bold;
+    font-size: 18px;
+  }
+")),
+  
+  title <- tags$div(
+    tag.map.title, HTML("To see allocations broken down, click your district")
+  ),
+  titlePanel(title="Leandro Budget Tool"),
+  sidebarLayout(
+    sidebarPanel(
+      helpText("View your districts anticipated funding
+               by current or projected ('27-28) year"),
+      
+      selectInput("var", 
+                  label = "Choose a year to display",
+                  choices = list("Current", 
+                                 "Current with Leandro",
+                                 "2027-2028 Projected", 
+                                 "2027-2028 with Leandro"),
+                  selected = "Current")),
+                mainPanel(leaflet(mergedshapeL) %>%
+                            addProviderTiles("MapBox", options = providerTileOptions(
+                              id = "mapbox.light",
+                              accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
+                            addPolygons(fillColor = ~pal(Total),
+                                        weight = .5,
+                                        opacity = 1,
+                                        color = "black",
+                                        fillOpacity = 0.7,
+                                        popup = paste(mergedshapeL$NAME, "<br>",
+                                                      "Textbooks", mergedshapeL$Textbks, "<br>",
+                                                      "At-risk", mergedshapeL$At_Risk, "<br>",
+                                                      "Classroom Supply/Mat.", mergedshapeL$C_S___M, "<br>",
+                                                      "LEP", mergedshapeL$Lmt_E_P, "<br>",
+                                                      "Chi. w/ Dis.", mergedshapeL$Chld__D))
+                                        %>%
+                                          addLegend(pal = pal, values = ~Total, opacity = 0.7, title = 'Total 21-22 Funding',
+                                                    position = "bottomright") %>%
+                            addTiles() %>%
+                            addControl(title, position = "topleft", className="map-title"),
+                         
+                          
+                          
+                          ),))
 
 server <-function(input, output){}
 
-
 shinyApp(ui, server)
 
-# map example (built on the standard North Carolina shapefile):
+#run shiny connect to publish
 
-library(leaflet)
-library(dplyr)
-library(Rcpp)
-library(sf)
-
-# NC counties - a shapefile shipped with the sf package
-#shape <- st_read("~/Users/sarahpedonti/Downloads/tl_2017_37_unsd/tl_2017_37_unsd.shp") %>% 
-#st_transform(shape, crs = 4326)
-#another way 
-#aoi_boundary_NCDPI <- st_read(
-#"~/Downloads/tl_2017_37_unsd/tl_2017_37_unsd.shp")
-
-library(ggplot2)
-ggplot() + 
-  geom_sf(data = aoi_boundary_NCDPI, size = 1, color = "grey", fill = "blue") + 
-  ggtitle("NCPI Boundary Plot") + 
-  coord_sf()
-
-#a third way #THIS WORKS, USE THIS
-library(tigris)
-library(leaflet)
-
-schools <- school_districts("North Carolina")
-
-schools <- write.csv(schools, "schools.csv")
-
-#get simple maps with popup for districtname
-leaflet(schools) %>%
-  addProviderTiles("HikeBike.HikeBike") %>%
-  addPolygons(fillColor = "white",
-              color = "black",
-              weight = 0.5, 
-              popup = paste("LEA", schools$NAME, "<br>",
-                            "Lowest Grade", schools$LOGRADE))
-
-# ProviderTiles options: "CartoDB.Positron", "Thunderforest.Landscape", "HikeBike.HikeBike"
-
-#now try to merge shapefile and LEANDRO so pop up boxes will have info
-#first need to add word "schools" to end of LEA in Leandro file (just did in Excel)
-
-#now merge shapefile and budget numbers for pop ups.THIS DOESN'T WORK
-
-mergedshapeL <- merge(Leandro, schools, by.x= "LEA", by.y= "NAME")
-
-# tryinganotherway STILL NOT WORKING
-#library(rgdal)
-#Leandro <- read.csv("~/Downloads/2021LeandroCostEstimateswv.csv")
-#myspdf <- readOGR("~/Downloads/tl_2017_37_unsd/tl_2017_37_unsd.shp")
-#mergedshapeL <- merge(Leandro, myspdf, by.x= "LEA", by.y= "NAME")
-
-library(raster)
-
-raster::shapefile(mergedshapeL, "mergedshapeL.shp")
-
-county_info <- read.csv("~/Downloads/2021LeandroCostEstimateswv_wGEOID.csv")
-county_shape <- read_sf("~/Downloads/tl_2017_37_unsd/tl_2017_37_unsd.shp")
+install.packages('rsconnect')
+library(rsconnect)
+rsconnect::setAccountInfo(name='sarahpedonti',
+                          token='0C7EF3A322A741CDDD35E5366C22897F',
+                          secret='JrUrKknfyeFCs0br4ytrYGg9T6HAikB1nBlo+f1o')
+#to deploy
+library(rsconnect)
+rsconnect::deployApp('path/to/your/app')
 
 
-library(sf)
-library(dplyr)
 
-county_info <- read.csv("~/rStuff/Leandro/2021LeandroCostEstimateswv_wGEOID.csv")
-county_info$GEOID <- as.character(county_info$GEOID)
-county_shape <- read_sf("~/Downloads/tl_2017_37_unsd/tl_2017_37_unsd.shp")
-county_info %>% full_join(y = county_shape, by = 'GEOID') %>%
-  st_as_sf() %>%
-  sf::st_write("~/Downloads/output.shp") 
 
-mergedshapeL <- read_sf("~/Downloads/output.shp")
 
-#save merge back out as shapefile for leaflet to read
-#library(raster)
-s#hapefile(mergedshapeL, "path/merged.shp")
+#setting up a continuous color palette for orange
+#f99d20
+#F9A636
+#FAB04C
+#FABA62
+#FBC479
+#FBCE8F
+#FCD7A5
+#FDE1BC
+#FDEBD2
+#FEF5E8
+#FEFFFF
 
-#now get map with additional finance info in popups
-leaflet(mergedshapeL) %>%
-  addProviderTiles("HikeBike.HikeBike") %>%
-  addPolygons(fillColor = "white",
-              color = "black",
-              weight = 0.5, 
-              popup = ~NAME)
+#setting up a continuous color palette for green
+#08ad8d
+#20B598
+#39BDA3
+#52C5AF
+#6ACDBA
+#83D5C6
+#9CDED1
+#B4E6DC
+#CDEEE8
+#E6F6F3
+#FEFEFF
+
+
+
+#colors for quick references
+  #navy = "#1E355E",
+  #green = "#08AD8D",
+  #orange = " #F99D20",
+
+
+
--- app.R
+++ app.R
@@ -1,49 +1,166 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#download Leandro data (only need if not working from Github and reading data locally)
+#Leandro <- read.csv("~/Downloads/2021LeandroCostEstimateswv.csv")
+read.csv("2021Leandrocostestimatesfullfund_wGEOID.csv")
+read.csv("2027Leandrocostestimateswfullfunding_wGEOID.csv")
+library(sf)
+library(dplyr)
+#readingin and merging shape file and finance info (no longer need after initial creation)
+#county_info <- read.csv("~/Leandro1/2021LeandroCostEstimateswv_wGEOID.csv")
+#county_info$GEOID <- as.character(county_info$GEOID)
+#county_shape <- read_sf("~/Leandro1/tl_2017_37_unsd.shp")
+#county_info %>% full_join(y = county_shape, by = 'GEOID') %>%
+  #st_as_sf() %>%
+  #sf::st_write("~/Leandro1/output.shp") 
+
+#reading in merged shape file that was written from above
+mergedshapeL <- read_sf("output.shp")
+
+#get rid of commas so that R can read as.numeric
+library(eeptools)
+mergedshapeL$Total <- decomma(mergedshapeL$Total)
+
 
 library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(Rcpp)
+library(htmlwidgets)
+library(htmltools)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+#set color bins for chloropeth mapping
+library(RColorBrewer)
+pal <- colorNumeric(
+  palette = "Blues", 
+  domain = mergedshapeL$Total)
+library(colorspace)
+library(rgdal)
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+#basic architecture for shiny app (with chloropeth mapping; change this to PPE?)
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+ui <- fluidPage(#theme="bootstrap.css",
+  tags$head(
+    tags$style(HTML(" @import url('http://fonts.cdnfonts.com/css/itc-avant-garde-gothic-std-book');
+      body {
+        background-color: #1E355E;
+        color: white;
+      }
+      /* Change font of header text */
+      h2 {
+          font-family: 'ITC Avant Garde Gothic Std Bold', sans-serif;
+      }
+      /* Make text visible on inputs */
+      .shiny-input-container {
+        color: #F99D20;
+      }"))
+  ),
+  tag.map.title <- tags$style(HTML("
+  .leaflet-control.map-title { 
+    transform: translate(-50%,20%);
+    position: fixed !important;
+    left: 50%;
+    text-align: center;
+    padding-left: 10px; 
+    padding-right: 10px; 
+    background: #1E355E;
+    font-weight: bold;
+    font-size: 18px;
+  }
+")),
+  
+  title <- tags$div(
+    tag.map.title, HTML("To see allocations broken down, click your district")
+  ),
+  titlePanel(title="Leandro Budget Tool"),
+  sidebarLayout(
+    sidebarPanel(
+      helpText("View your districts anticipated funding
+               by current or projected ('27-28) year"),
+      
+      selectInput("var", 
+                  label = "Choose a year to display",
+                  choices = list("Current", 
+                                 "Current with Leandro",
+                                 "2027-2028 Projected", 
+                                 "2027-2028 with Leandro"),
+                  selected = "Current")),
+                mainPanel(leaflet(mergedshapeL) %>%
+                            addProviderTiles("MapBox", options = providerTileOptions(
+                              id = "mapbox.light",
+                              accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
+                            addPolygons(fillColor = ~pal(Total),
+                                        weight = .5,
+                                        opacity = 1,
+                                        color = "black",
+                                        fillOpacity = 0.7,
+                                        popup = paste(mergedshapeL$NAME, "<br>",
+                                                      "Textbooks", mergedshapeL$Textbks, "<br>",
+                                                      "At-risk", mergedshapeL$At_Risk, "<br>",
+                                                      "Classroom Supply/Mat.", mergedshapeL$C_S___M, "<br>",
+                                                      "LEP", mergedshapeL$Lmt_E_P, "<br>",
+                                                      "Chi. w/ Dis.", mergedshapeL$Chld__D))
+                                        %>%
+                                          addLegend(pal = pal, values = ~Total, opacity = 0.7, title = 'Total 21-22 Funding',
+                                                    position = "bottomright") %>%
+                            addTiles() %>%
+                            addControl(title, position = "topleft", className="map-title"),
+                         
+                          
+                          
+                          ),))
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
+server <-function(input, output){}
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
+
+#run shiny connect to publish
+
+#library(rsconnect)
+#rsconnect::setAccountInfo(name='sarahpedonti',
+                          #token='0C7EF3A322A741CDDD35E5366C22897F',
+                          #secret='JrUrKknfyeFCs0br4ytrYGg9T6HAikB1nBlo+f1o')
+#to deploy
+library(rsconnect)
+rsconnect::deployApp('~/Leandro1/app.R')
+
+
+
+
+
+#setting up a continuous color palette for orange
+#f99d20
+#F9A636
+#FAB04C
+#FABA62
+#FBC479
+#FBCE8F
+#FCD7A5
+#FDE1BC
+#FDEBD2
+#FEF5E8
+#FEFFFF
+
+#setting up a continuous color palette for green
+#08ad8d
+#20B598
+#39BDA3
+#52C5AF
+#6ACDBA
+#83D5C6
+#9CDED1
+#B4E6DC
+#CDEEE8
+#E6F6F3
+#FEFEFF
+
+
+
+#colors for quick references
+  #navy = "#1E355E",
+  #green = "#08AD8D",
+  #orange = " #F99D20",
+
+
+
