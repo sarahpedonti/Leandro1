5c5ba96d31679107fbaae9be7dc4b44b81d6998b
sarahpedonti <86248316+sarahpedonti@users.noreply.github.com>
2021-09-07 08:58:02 -0400

    drop downs
    updated drop down bars

--- app.R
+++ app.R
@@ -1,30 +1,41 @@
 #download Leandro data (only need if not working from Github and reading data locally)
 #Leandro <- read.csv("~/Downloads/2021LeandroCostEstimateswv.csv")
-read.csv("2021Leandrocostestimatesfullfund_wGEOID.csv")
-read.csv("2027Leandrocostestimateswfullfunding_wGEOID.csv")
+`2021-2022 Projected` <-read.csv("2021Leandrocostestimateswv_wGEOID.csv")
+`2021-2022 with Leandro` <- read.csv("2021Leandrocostestimatesfullfund_wGEOID.csv")
+`2027-2028 with Leandro` <- read.csv("2027Leandrocostestimateswfullfunding_wGEOID.csv")
+
 library(sf)
 library(dplyr)
+
+#added this in to make district comparisons clearer
+#thispart is done.
+#ADM <- read.csv("~/Downloads/Copy of 21-22 Allotted ADM.csv")
+#merge ADM and shapefile from below
+
+#mergedshapeL2 <- merge(ADM, mergedshapeL, by.x = "Name", by.y = "LEA")
+#adding PPE variable
+#maketotalADM numeric
+library(eeptools)
+#mergedshapeL2$TOTAL <- decomma(mergedshapeL2$TOTAL)
+#mergedshapeL2$TOTAL <- as.numeric(mergedshapeL2$TOTAL)
+#mergedshapeL2$"Per Pupil Funding" <- mergedshapeL2$Total/mergedshapeL2$TOTAL
+#mergedshapeL2 <- mergedshapeL2[, -c(1:17)] # delete columns 2 through 17
+#mergedshapeL2 <- mergedshapeL2[, -c(33:50)] # delete columns 33 through 50
+#mergedshapeL2 <- mergedshapeL2[, -c(33:42)] # delete columns 33 through 42
+
 #readingin and merging shape file and finance info (no longer need after initial creation)
 #county_info <- read.csv("~/Leandro1/2021LeandroCostEstimateswv_wGEOID.csv")
 #county_info$GEOID <- as.character(county_info$GEOID)
 #county_shape <- read_sf("~/Leandro1/tl_2017_37_unsd.shp")
-#county_info %>% full_join(y = county_shape, by = 'GEOID') %>%
-  #st_as_sf() %>%
-  #sf::st_write("~/Leandro1/output.shp") 
+#county_info %>% full_join(y = county_shape, by = 'GEOID') 
+#mergedshapeL2%>%
+#st_as_sf() %>%
+#sf::st_write("~/Leandro1/output1.shp") 
 
 #reading in merged shape file that was written from above
-mergedshapeL <- read_sf("output.shp")
+mergedshapeL <- read_sf("output1.shp")
 
-
-#making labels for map below
-labels <- sprintf(
-  mergedshapeL$LEA
-) %>% 
-  lapply(htmltools::HTML)
-
-#get rid of commas so that R can read as.numeric
-library(eeptools)
-mergedshapeL$Total <- decomma(mergedshapeL$Total)
+mergedshapeL <- rename(mergedshapeL, "Per Pupil Funding" = PrPplFn)
 
 
 library(shiny)
@@ -34,11 +45,19 @@ library(Rcpp)
 library(htmlwidgets)
 library(htmltools)
 
+#making labels for map below
+labels <- sprintf(
+  mergedshapeL$NAME
+) %>% 
+  lapply(htmltools::HTML)
+
+
+
 #set color bins for chloropeth mapping
 library(RColorBrewer)
 pal <- colorNumeric(
   palette = "Blues", 
-  domain = mergedshapeL$Total)
+  domain = mergedshapeL$`Per Pupil Funding`)
 library(colorspace)
 library(rgdal)
 
@@ -78,41 +97,87 @@ ui <- fluidPage(#theme="bootstrap.css",
 ")),
   
   title <- tags$div(
-    tag.map.title, HTML("To see allocations broken down, click your district")
+    tag.map.title, HTML("Click your district of interest to view breakdown of allocations.")
   ),
   titlePanel(title="Leandro Budget Tool"),
 
   
 sidebarLayout(
     sidebarPanel(
-        helpText("View your district's anticipated funding
-               by current ('21-22) or projected ('27-28) year"),
+        helpText("North Carolina school districts/LEAs are displayed according to geographical location.
+Each LEA contains pop up boxes with budget categories calculated for either the 21-22 or 27-28 school yearc
+The dropdown box below will allow you to select the year.First, choose your year.Then,click your district of interest to view breakdown of allocations,
+                 or select an allocation category from the box below to see a graph of categories"),
       
-      selectInput("var", 
+      selectInput("dataset", 
                   label = "Choose a year to display",
-                  choices = list("2021-2022 Projected", 
-                                 "2021-2022 with Leandro",
-                                 "2027-2028 Projected", 
-                                 "2027-2028 with Leandro"),
+                  choices = c("2021-2022 Projected" = "2021-2022 Projected", 
+                                 "2021-2022 with Leandro" = "2021-2022 with Leandro",
+                                 "2027-2028 with Leandro"= "2027-2028 with Leandro"),
+                  selected = "2021-2022 Projected"),
+      selectInput("var", 
+                  label = "Choose a an allottment category",
+                  choices = c("Textbooks" = "Textbooks",
+                              "At Risk" = "At.Risk",
+                              "Classroom Supplies"= "Classroom.Supplies...Materials",
+                              "LEP" = "Limited.English.Proficiency", 
+                              "Children with Dis." = "Children.with.Disabilities", 
+                              "Low Wealth" = "Low.Wealth", 
+                              "Teacher Assistants" = "Teacher.Assistants", 
+                              "Supp. Funding" = "Disadvantaged.Students.Supplemental.Funding", 
+                              "Instructional Support" = "Instructional.Support.Dollars", 
+                              "Assistant Principals" = "Assistant.Principals.Dollars", 
+                              "Non-Instructional" = "Non-Instructional Supports", 
+                              "Central Office" = "Central.Office", 
+                              "Professional Development" = "Professional.Development...Mentoring", 
+                              "Community School Coord." =  "Community.School.Coordinator..Dollars.",
+                              "NC Virtual" = "NC.Virtual.Public.Dollars", 
+                              "Career Development Coordinators" = "Career.Development.Coordinators..Dollar.", 
+                              "Classroom Teacher" = "Classroom.Teachers..Dollar.", 
+                              "Principals" = "Principals...Dollar.", 
+                              "Transportation" = "Transportation", 
+                              "Small County" = "Small County", 
+                              "CTE Teachers" = "CTE.Teachers..Dollar.", 
+                              "CTE Program Supports" = "CTE.Program.Support", 
+                              "Driver Training" = "Driver Training", 
+                              "Total" = "Total"
+                              ),
                   selected = "2021-2022 Projected")),
-                mainPanel(mergedshapeL %>%
+                mainPanel(
+                  
+                  mergedshapeL %>%
                             sf::st_transform(4326) %>% 
                             leaflet() %>%
                             addProviderTiles("MapBox", options = providerTileOptions(
                               id = "mapbox.light",
                               accessToken = 'pk.eyJ1Ijoic2ZmODA5IiwiYSI6ImNrc2ZocmQwZTFhZ2kyb255eTdqZnhraDQifQ.R6fe2zZx8SVrWtYSZjfGrg
 ')) %>%
-                            addPolygons(fillColor = ~pal(Total),
+                            addPolygons(fillColor = ~pal(`Per Pupil Funding`),
                                         weight = .5,
                                         opacity = 1,
                                         color = "black",
                                         fillOpacity = 0.7,
                                         popup = paste(
-                                                      "Textbooks", mergedshapeL$Textbks, "<br>",
-                                                      "At-risk", mergedshapeL$At_Risk, "<br>",
-                                                      "Classroom Supply/Mat.", mergedshapeL$C_S___M, "<br>",
-                                                      "LEP", mergedshapeL$Lmt_E_P, "<br>",
-                                                      "Chi. w/ Dis.", mergedshapeL$Chld__D), 
+                                                      "Textbooks:", mergedshapeL$Textbks, "<br>",
+                                                      "At-risk:", mergedshapeL$At_Risk, "<br>",
+                                                      "Classroom Supply/Mat.:", mergedshapeL$C_S___M, "<br>",
+                                                      "LEP:", mergedshapeL$Lmt_E_P, "<br>",
+                                                      "Chi. w/ Dis.:", mergedshapeL$Chld__D, "<br>", 
+                                                      "Low Wealth:", mergedshapeL$Lw_Wlth, "<br>",
+                                                      "Teacher Assist.:", mergedshapeL$Tchr_As, "<br>",
+                                                      "Supp. Funding:", mergedshapeL$D_S_S_F, "<br", 
+                                                      "Instructional Support:", mergedshapeL$I_S__D_, "<br>", 
+                                                      "Assistant Principal:", mergedshapeL$A_P__D_, "<br>", 
+                                                      "Non-Inst. Support:", mergedshapeL$Nn_In_S, "<br>", 
+                                                      "Central Office:", mergedshapeL$Cntrl_O, "<br>",
+                                                      "NC Virtual:", mergedshapeL$NC_V_P_, "<br>",
+                                                      "Classroom Teachers:", mergedshapeL$C_T___D, "<br>",
+                                                      "Principals:", mergedshapeL$Pr___D_, "<br>", 
+                                                      "Transportation:", mergedshapeL$Trnsprt, "<br>",
+                                                      "Small County:", mergedshapeL$Smll_Cn, "<br>", 
+                                                      "CTE Teachers:", mergedshapeL$CTE_T__D, "<br>",
+                                                      "CTE Prog. Supp.:", mergedshapeL$CTE_P_S, "<br>", 
+                                                      "Driver Training:", mergedshapeL$Drvr_Tr, "<br>"), 
                                         highlight = highlightOptions(weight = 5,
                                                                      color = "#666",
                                                                      dashArray = NULL,
@@ -125,24 +190,39 @@ sidebarLayout(
                                                                     direction = "auto")) %>%
         addLegend("bottomright",
                   pal = pal,
-                  values = mergedshapeL$Total,
+                  values = mergedshapeL$`Per Pupil Funding`,
                   title = "Total Funding",
                   opacity = 1) %>%
                          
         addTiles() %>%
-        addControl(title, position = "topleft", className="map-title")
+        addControl(title, position = "topleft", className="map-title"),
                           
                           
-                          )),)
+        plotOutput("plotfunding")                 )))
 
-server <-function(input, output){output$plot1 <- renderPlot({
-  x <- rnorm(input$n)  
-  bw <- diff(range(x))/50
-  ggplot(mergedshapeL(x=LEA), aes(Total)) +
-    geom_histogram(color = "blue", fill = "white", binwidth = bw) + 
-    labs(x="LEA", y="Counts")})}
+server <-function(input, output){
+
+  function(input, output, session) {
+    
+    data1 <- reactive({
+      input$`2021-2022 with Leandro`
+    })
+    data2 <- reactive({
+      input$`2027-2028 with Leandro`
+    })
+    
+    output$plotfunding <- renderPlot({
+      boxplot(get(data2()) ~ get(data1()) )
+    })
+    
+    output$Hist <- renderPlot({
+      req(data1())
+      hist(data1())})}}
+    
 
 shinyApp(ui, server)
+
+
 
 #run shiny connect to publish
 
